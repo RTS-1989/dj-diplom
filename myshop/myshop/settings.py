1a38a5dc3c25fbf4010a3fb31fe4dfd822019746
Ruslan <russibg@gmail.com>
2020-11-23 19:56:06 +0500

    Added new app for orders. Added views, forms, models, urls, tests for orders app. Added admin panel for orders app. 2 migrations was applied for models in orders app. Urls for cart, myshop changed. Views for cart was changed. Settings changed, added new app. Base, index and detail(cart app) templates was changed.

--- myshop/myshop/settings.py
+++ myshop/myshop/settings.py
@@ -60,6 +60,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'shop.apps.ShopConfig',
     'cart.apps.CartConfig',
+    'orders.apps.OrdersConfig',
 ]
 
 MIDDLEWARE = [
@@ -85,6 +86,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'cart.context_processors.cart',
             ],
         },
     },
