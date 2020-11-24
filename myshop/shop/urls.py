b8df852d5eb3a200f9fe1c9536537143694115fa
Ruslan <russibg@gmail.com>
2020-11-24 22:34:13 +0500

    Added new view (shop app) for smartphones.html. Template for smartphones.html was changed. Added new path for new template and view in myshop/urls.py.

--- myshop/shop/urls.py
+++ myshop/shop/urls.py
@@ -7,6 +7,7 @@ app_name = 'shop'
 urlpatterns = [
     path('', views.product_list, name='product_list'),
     path('index.html', views.product_list, name='product_list'),
+    path('smartphones.html', views.smartphones_list, name='smartphones_list'),
     path('<slug:category_slug>', views.product_list,
          name='product_list_by_category'),
     path('catalog/<int:id>/<slug:slug>/', views.product_detail,
--- myshop/shop/views.py
+++ myshop/shop/views.py
@@ -22,6 +22,25 @@ def product_list(request, category_slug=None):
     return render(request, template, context)
 
 
+def smartphones_list(request, category_slug=None):
+    category = None
+    categories = Category.objects.all()
+    products = Product.objects.filter(available=True)
+    template = 'shop/smartphones.html'
+
+    if category_slug:
+        category = get_object_or_404(Category, slug=category_slug)
+        products = products.filter(category=category)
+
+    context = {
+        'category': category,
+        'categories': categories,
+        'products': products
+    }
+
+    return render(request, template, context)
+
+
 def product_detail(request, id, slug):
     product = get_object_or_404(Product, id=id,
                                 slug=slug, available=True)
