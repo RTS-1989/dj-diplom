afb5bc6a86dfa30bc3907c2bcbd5c3b0c1aa3f2a
Ruslan <russibg@gmail.com>
2020-11-26 00:13:52 +0500

    Added pagination. Added 3 products to db. Changed views.py (shop app), changed smartphones.html, added pagination.

--- myshop/shop/views.py
+++ myshop/shop/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, get_object_or_404
 from shop.models import Category, Product
 from cart.forms import CartAddProductForm
+from django.core.paginator import Paginator
+from urllib.parse import urlencode
+from django.urls import reverse
 
 
 def product_list(request, category_slug=None):
@@ -27,15 +30,28 @@ def smartphones_list(request, category_slug=None):
     categories = Category.objects.all()
     products = Product.objects.filter(available=True)
     template = 'shop/smartphones.html'
+    paginator = Paginator(products, 5)
+    current_page = int(request.GET.get('page', 1))
+    next_page_url, previous_page_url = None, None
+    current_products = paginator.get_page(current_page)
+    if current_products.has_next():
+        next_page = urlencode({'page': current_products.next_page_number()})
+        next_page_url = f'?{next_page}'
+    if current_products.has_previous():
+        previous_page = urlencode({'page': current_products.previous_page_number()})
+        previous_page_url = f'?{previous_page}'
 
     if category_slug:
         category = get_object_or_404(Category, slug=category_slug)
-        products = products.filter(category=category)
+        current_products = current_products.filter(category=category)
 
     context = {
         'category': category,
         'categories': categories,
-        'products': products
+        'products': current_products,
+        'current_page': current_page,
+        'prev_page_url': previous_page_url,
+        'next_page_url': next_page_url,
     }
 
     return render(request, template, context)
